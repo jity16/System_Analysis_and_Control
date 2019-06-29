a85b7fc0be4ed332e904e83d1c888749748d5e95
嵇天颖 <jity16@mails.tsinghua.edu.cn>
2019-06-30 04:16:44 +0800

    g1 is ok

---
+++ src/g1_bode.m
@@ -0,0 +1,8 @@
+% a = 0.01,0.1,1,10,100
+a = 100;
+num=[1/a,1];
+den=[1,1,1] 
+G=tf(num,den)                       
+bode(G) 
+grid on
+title(sprintf('系统频率响应 a = %.2f',a));
---
+++ src/g1_step.m
@@ -0,0 +1,8 @@
+% a = 0.01,0.1,1,10,100
+a = 0.01;
+num=[1/a,1] % num = [1/a,1]
+den=[1,1/a+1,2] %den = [1,1/a+1,2]
+step(num,den)
+grid on
+xlabel('t'),ylabel('c(t)')
+title(sprintf('系统单位阶跃响应 a = %.2f',a));
